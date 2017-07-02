647caea7c231182db0b18e3a866966fd3d78f598
Dominik Moser <dominik.moser@inso.tuwien.ac.at>
2017-07-03 01:37:00 +0200

    add cleanup

--- build.sh
+++ build.sh
@@ -4,4 +4,14 @@ pdflatex ktt.tex
 pdflatex ktt.tex
 pdflatex ktt.tex
 
-pdfpc --disable-cache --last-minutes=5 --duration=20 ktt.pdf
+rm *.pdfpc
+rm *.aux
+rm *.log
+rm *.nav
+rm *.out
+rm *.snm
+rm *.toc
+rm *.synctex.gz
+rm *.vrb
+
+pdfpc --disable-compression --last-minutes=5 --duration=20 ktt.pdf
